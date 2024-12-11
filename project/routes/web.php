ac3141c9f3f1c205c5722231f1511fb7b1004d1f
HGThinh <132908713+HGThinh@users.noreply.github.com>
2024-12-11 11:21:44 +0100

    Add route and middleware for admin
    Create multiple routes for admin
    Get: getPost, getComment, getApplication, getReport, getUser
    Post: addPost, addComment,, addReport
    Put: updateUserProfile
    Delete: deleteUser, deletePost, deleteComment

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -24,4 +25,26 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+// Admin - GET routes (Requires login and admin access)
+Route::middleware(['auth', 'isAdmin'])->group(function () {
+    Route::get('/admin/users', [AdminController::class, 'getUsers']); // Retrieve user's list
+    Route::get('/admin/reports', [AdminController::class, 'getReports']); // Retrieve reports
+    Route::get('/admin/posts', [AdminController::class, 'getPosts']); // Retrieve posts
+    Route::get('/admin/comments', [AdminController::class, 'getComments']); // Retrieve comments
+    Route::get('/admin/applications', [AdminController::class, 'getApplications']); // Retrieve applications
+
+    // Admin - POST routes (Requires login and admin access)
+    Route::post('/admin/posts', [AdminController::class, 'addPost']); // Add new post
+    Route::post('/admin/comments', [AdminController::class, 'addComment']); // Add new comment
+    Route::post('/admin/reports', [AdminController::class, 'addReport']); // Add new report
+
+    // Admin - PUT routes (Requires login and admin access)
+    Route::put('/admin/users/{id}', [AdminController::class, 'updateUserProfile']); // Update user's profile
+
+    // Admin - DELETE routes (Requires login and admin access)
+    Route::delete('/admin/users/{id}', [AdminController::class, 'deleteUser']); // Delete user
+    Route::delete('/admin/posts/{id}', [AdminController::class, 'deletePost']); // Delete post
+    Route::delete('/admin/comments/{id}', [AdminController::class, 'deleteComment']); // Delete comment
+});
+
+require __DIR__ . '/auth.php';
