76a893ea0298f6718fb65c908a294c8508c9466d
HGThinh <132908713+HGThinh@users.noreply.github.com>
2024-12-11 11:41:06 +0100

    Add is_admin to users table
    Add boolean is_admin to check if the user is an admin in database

--- project/app/Http/Controllers/AdminController.php
+++ project/app/Http/Controllers/AdminController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+
+use App\Models\User;
 
 class AdminController extends Controller
 {
-    //
+    // Ensure this method is defined
+    public function getUsers()
+    {
+        // Fetch all users (or use any specific query you need)
+        $users = User::all();
+
+        // Return a view with the users
+        return view('users', compact('users'));
+    }
 }
