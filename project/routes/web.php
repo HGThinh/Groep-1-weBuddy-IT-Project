8a58c7115bacb2d69df0f92b69bd64eeee901497
Selma <145977452+SelmaElMahyaoui@users.noreply.github.com>
2024-12-11 11:11:18 +0100

    added the route to nevigate to the resource page

--- project/routes/web.php
+++ project/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\ResourceController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -23,5 +24,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/', function () {
+    return Inertia::render('Welcome'); // Home page
+})->name('home');
+
+Route::get('/resources', [ResourceController::class, 'index'])->name('resources');
+
 
 require __DIR__.'/auth.php';
