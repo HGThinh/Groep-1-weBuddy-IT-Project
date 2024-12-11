51e47b8b1f24e6c52f12a4cff0944c010a11bddb
Selma <145977452+SelmaElMahyaoui@users.noreply.github.com>
2024-12-11 15:48:26 +0100

    resources added temporarly to see on browser and base resource component

---
+++ project/resources/js/Components/ResourceComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from '../styles/Resources.module.css'
+
+const ResourceComponent = ({ title, type, description, tags }) => {
+    return 
+}
+export default ResourceComponent;
