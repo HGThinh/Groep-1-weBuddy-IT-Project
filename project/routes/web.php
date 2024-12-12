cb5d3d257aa86e785be9da5406ed5c46a2f6ff0a
HGThinh <132908713+HGThinh@users.noreply.github.com>
2024-12-12 10:22:58 +0100

    Add more route for user side

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\AboutUsController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MentorController;
+use App\Http\Controllers\ForumController;
+use App\Http\Controllers\PointsController;
+use App\Http\Controllers\ResourcesController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -20,26 +26,60 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    //Profile route
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //Home route
+    Route::get('/home', [HomeController::class, 'get'])->name('home.get'); // Retrieve home
+
+    //Course route
+    Route::get('/course/{id}', [HomeController::class, 'get'])->name('course.get'); // Retrieve course by id
+
+    //Mentor route
+    Route::get('/mentor', [MentorController::class, 'getMentor'])->name('mentor.get'); // Retrieve mentor
+    Route::get('/mentor/info', [MentorController::class, 'getInfo'])->name('mentor.info.get'); // Retrieve mentor
+    Route::get('/mentor/profile/{id}', [MentorController::class, 'getMentorProfile'])->name('mentor.profile.get'); // Retrieve mentor by id
+    Route::post('/mentor/request', [MentorController::class, 'sendRequest'])->name('mentor.request.post'); // Send new mentorship request
+    Route::post('/mentor/application', [MentorController::class, 'sendApplication'])->name('mentor.application.post');; // Send mentorship application
+
+    //Forum route
+    Route::get('/forum', [ForumController::class, 'getForum'])->name('forum.get'); // Retrieve forum
+    Route::get('/forum/post/{id}', [ForumController::class, 'getPost'])->name('forum.post.get'); // Retrieve forum by id
+    Route::post('/forum/ask', [ForumController::class, 'sendQuestion'])->name('forum.ask.post');; // Ask new question
+    Route::post('/report', [ForumController::class, 'sendReport'])->name('report.post');; // Send report this post
+    Route::put('/forum/post/{id}', [ForumController::class, 'updatePost'])->name('forum.post.update'); // Update post
+    Route::delete('/forum/post/{id}', [ForumController::class, 'deletePost'])->name('forum.post.delete'); // Delete post
+
+    //Resources route
+    Route::get('/resources', [ResourcesController::class, 'getResources'])->name('resources.get'); // Retrieve resources
+    Route::get('/resources/post/{id}', [ResourcesController::class, 'getResourcesPost'])->name('resources.post.get'); // Retrieve resources by id
+    Route::post('/resources/add', [ResourcesController::class, 'sendResources'])->name('resources.post.post');; // Ask new question
+    Route::put('/resources/post/{id}', [ResourcesController::class, 'updateResourcesPost'])->name('resources.post.update'); // Update post
+    Route::delete('/resources/post/{id}', [ResourcesController::class, 'deleteResources'])->name('resources.post.delete'); // Delete post
+
+    //Points route
+    Route::get('/points', [PointsController::class, 'getPoints'])->name('points.get'); // Retrieve points
+
+    //About us route
+    Route::get('/aboutus', [AboutUsController::class, 'getAboutus'])->name('aboutus.get'); // Retrieve about us
+    Route::post('/aboutus', [AboutUsController::class, 'sendIssue'])->name('aboutus.issue.post');; // Send issue
+
 });
 
 // Admin - GET routes (Requires login and admin access)
 Route::middleware(['auth', 'isAdmin'])->group(function () {
+    // Admin - POST routes (Requires login and admin access)
+
+
     Route::get('/admin/users', [AdminController::class, 'getUsers']); // Retrieve user's list
     Route::get('/admin/reports', [AdminController::class, 'getReports']); // Retrieve reports
     Route::get('/admin/posts', [AdminController::class, 'getPosts']); // Retrieve posts
     Route::get('/admin/comments', [AdminController::class, 'getComments']); // Retrieve comments
     Route::get('/admin/applications', [AdminController::class, 'getApplications']); // Retrieve applications
 
-    // Admin - POST routes (Requires login and admin access)
-    Route::post('/admin/posts', [AdminController::class, 'addPost']); // Add new post
-    Route::post('/admin/comments', [AdminController::class, 'addComment']); // Add new comment
-    Route::post('/admin/reports', [AdminController::class, 'addReport']); // Add new report
 
-    // Admin - PUT routes (Requires login and admin access)
-    Route::put('/admin/users/{id}', [AdminController::class, 'updateUserProfile']); // Update user's profile
+
 
     // Admin - DELETE routes (Requires login and admin access)
     Route::delete('/admin/users/{id}', [AdminController::class, 'deleteUser']); // Delete user
