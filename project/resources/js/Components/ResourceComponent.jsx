fcf3ed0a5874ebfde0df75edcb559abc8bd73f87
Selma <145977452+SelmaElMahyaoui@users.noreply.github.com>
2024-12-12 09:41:55 +0100

    test

--- project/resources/js/Components/ResourceComponent.jsx
+++ project/resources/js/Components/ResourceComponent.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from '../styles/Resources.module.css'
 
 const ResourceComponent = ({ title, type, description, tags }) => {
-    return 
+    return (
+      <div className="ressource-card"></div>
+    )
 }
 export default ResourceComponent;
