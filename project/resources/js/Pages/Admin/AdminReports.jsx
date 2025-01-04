c656dcbbabdf17a1192286a207a8c09e5e34daa4
miladnnesim <milad.nesim@student.ehb.be>
2025-01-04 22:07:05 +0100

    Admin Reports page created

---
+++ project/resources/js/Pages/Admin/AdminReports.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import AdminNavbar from "@/Components/Admin/AdminNavbar";
+import Styles from "@/Components/Admin/AdminReportsPage.module.css";
+
+const App = () => {
+    return (
+        <>
+            <main>
+                <AdminNavbar />
+                <div className={Styles.container}>
+                    <div className={Styles["cards-container"]}>
+                        <a href="#link to forum reports">
+                            <div className={Styles.card}>
+                                <h1>Forum reports</h1>
+                                <div
+                                    className={`${Styles.icon} ${Styles["forum-icon"]}`}
+                                ></div>
+                            </div>
+                        </a>
+                        <a href="#link to forum reports">
+                            <div className={Styles.card}>
+                                <h1>Resources reports</h1>
+                                <div
+                                    className={`${Styles.icon} ${Styles["resources-icon"]}`}
+                                ></div>
+                            </div>
+                        </a>
+                        <a href="#link to forum reports">
+                            <div className={Styles.card}>
+                                <h1>Mentor reports</h1>
+                                <div
+                                    className={`${Styles.icon} ${Styles["mentor-icon"]}`}
+                                ></div>
+                            </div>
+                        </a>
+                        <a href="#link to forum reports">
+                            <div className={Styles.card}>
+                                <h1>Forum reports</h1>
+                                <div
+                                    className={`${Styles.icon} ${Styles["forum-icon"]}`}
+                                ></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </main>
+        </>
+    );
+};
+
+export default App;
