b9f3061aa0b8459e061828f1d151bfe35fda9975
HGThinh <132908713+HGThinh@users.noreply.github.com>
2025-01-07 01:53:34 +0100

    Update QuestionForum.jsx

--- project/resources/js/Components/QuestionForum.jsx
+++ project/resources/js/Components/QuestionForum.jsx
@@ -1,59 +1,44 @@
-import React from "react";
-import styles from "./QuestionForum.module.css";
+import React from 'react';
+import styles from './QuestionForum.module.css';
 
-// Question component
-const Question = ({
-    Name,
-    ProfilePicture,
-    TimePosted,
-    Catgeory,
-    QuestionTitle,
-    BoolAnswered,
-    tags,
-    Question,
-    upvotes,
-    comments,
-}) => {
+// Individual Question component
+const Question = ({ data }) => {
     return (
         <div className={styles.Question}>
             <div>
                 <div className={styles.containernameimage}>
                     <div className={styles.containerImg}>
                         <img
-                            src={ProfilePicture}
-                            alt={`${Name}'s profile`}
-                            onError={(e) =>
-                                (e.target.src = "Images/UserProfileNoPic.png")
-                            } // Fallback image
+                            src="/Images/UserProfileNoPic.png"
+                            alt="Default profile"
+                            onError={(e) => (e.target.src = "/Images/UserProfileNoPic.png")}
                         />
                     </div>
                     <h2>
-                        {Name} | Posted {TimePosted} ago in <a>{Catgeory}</a>
+                        Anonymous | Posted {data.created_at} ago in <a>{data.category}</a>
                     </h2>
                 </div>
                 <ul className={styles.tags}>
                     <li className={styles.Anwsered}>
-                        {BoolAnswered ? "Answered" : "Not Answered"}
+                        Not Answered
                     </li>
-                    {tags.map((tag, index) => (
+                    {data.tags.map((tag, index) => (
                         <li className={styles.tag} key={index}>
                             {tag}
                         </li>
                     ))}
                 </ul>
-                <h3>{QuestionTitle}</h3>
-                <p className={styles.textbox}>{Question}</p>
+                <h3>{data.title}</h3>
+                <p className={styles.textbox}>{data.content}</p>
                 <p className={styles.footerQuestion}>
                     <a href="">
-                        <img src="Images/arrow-circle-up.svg" alt="" />{" "}
-                        {upvotes}{" "}
+                        <img src="/Images/arrow-circle-up.svg" alt="" /> 0
                     </a>
                     <a href="">
-                        <img src="Images/message-circle.svg" alt="" />
-                        {comments}
+                        <img src="/Images/message-circle.svg" alt="" /> 0
                     </a>
                     <a href="">
-                        <img src="Images/flag-1.svg" alt="" /> Report
+                        <img src="/Images/flag-1.svg" alt="" /> Report
                     </a>
                 </p>
             </div>
@@ -65,8 +50,8 @@ const Question = ({
 const QuestionForum = ({ data }) => {
     return (
         <div className={styles.QuestionForum}>
-            {data.map((question, index) => (
-                <Question key={index} {...question} />
+            {data.map((question) => (
+                <Question key={question.id} data={question} />
             ))}
         </div>
     );
