b9e1311c8279c5bc059e867f87e619294def17f6
HGThinh <132908713+HGThinh@users.noreply.github.com>
2025-01-08 05:37:21 +0100

    Update VerifyEmailController.php

--- project/app/Http/Controllers/Auth/VerifyEmailController.php
+++ project/app/Http/Controllers/Auth/VerifyEmailController.php
@@ -5,23 +5,24 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
-use Illuminate\Http\RedirectResponse;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class VerifyEmailController extends Controller
 {
     /**
      * Mark the authenticated user's email address as verified.
      */
-    public function __invoke(EmailVerificationRequest $request): RedirectResponse
+    public function __invoke(EmailVerificationRequest $request): Response
     {
         if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
+            return Inertia::render('Aboutus');
         }
 
         if ($request->user()->markEmailAsVerified()) {
             event(new Verified($request->user()));
         }
 
-        return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
+        return Inertia::render('Aboutus');
     }
 }
